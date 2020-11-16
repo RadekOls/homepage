58a8ebef52789ab539290677292e97e4d0918949
Radek Olszewski <olszewski.radoslaw15@gmail.com>
2020-11-16 18:31:58 +0100

    navigation flex-center'

--- main.js
+++ main.js
@@ -1,2 +1,11 @@
-const change_footer = document.querySelector('.footer__describe--js');
-change_footer.innerHTML=`Radosław Olszewski Web Developer`;
+// const change_footer = document.querySelector('.footer__describe--js');
+// change_footer.innerHTML=`Radosław Olszewski Web Developer`;
+
+
+
+function welcome (name, age){
+console.log('Nazywam się ${name} i mam ${age}')
+
+}
+
+welcome(Radek; 34)
