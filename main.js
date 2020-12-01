aa4d12e8e52cd016ae0f51fc06117640af866a8c
Radek Olszewski <olszewski.radoslaw15@gmail.com>
2020-12-01 18:56:15 +0100

    new background-gradient

--- main.js
+++ main.js
@@ -2,10 +2,21 @@
 // change_footer.innerHTML=`Radosław Olszewski Web Developer`;
 
 
+// function createContent(querySelectorContent, content){
+// const element = document.querySelector(querySelectorContent);
+// element.innerHTML = content;
+// }
 
-function welcome (name, age){
-console.log('Nazywam się ${name} i mam ${age}')
+// createContent('.footer__describe--js',  'Nazywam się Piotr')
+// createContent('.header__main--js',  'Nauka porgramowania z Maćkiem z CTF')
+
+// const age = 35
+// const name = 'Radek'
+
+const welcome = (name, age) => {
+
+    console.log(`Nazywam się ${name} i mam ${age} lata`);
 
 }
 
-welcome(Radek; 34)
+welcome(name, age);
